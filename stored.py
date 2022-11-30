8d1b368cb16100f7093b2828702964c153e70345
Miles Pei <mpei36@gmail.com>
2022-11-30 09:46:43 -0600

    Added user input

--- stored.py
+++ stored.py
@@ -9,16 +9,23 @@ mydb = mysql.connector.connect(
 
 cursor = mydb.cursor()
 
+
 def bookFlight(first_name, last_name, email, phone, flight_id, seat_id):
-    cursor.callproc('bookFlight', (first_name, last_name, email, phone, flight_id, seat_id))
+    cursor.callproc('bookFlight', (first_name, last_name,
+                    email, phone, flight_id, seat_id))
+
 
 def manageTrip(passenger_id):
     cursor.callproc('manageTrip', (passenger_id))
 
+# Need support for checking flight status with flight id
 def checkFlightStatus(departure_city, arrival_city, departure_date):
-    cursor.callproc('checkFlightStatus', (departure_city, arrival_city, departure_date))
+    cursor.callproc('checkFlightStatus',
+                    (departure_city, arrival_city, departure_date))
+
 
 def displayScreen():
+    print("------------------------------------")
     print("Welcome to the flight booking system")
     print("1. Book a flight")
     print("2. Manage Trip/Check-in")
@@ -28,15 +35,35 @@ def displayScreen():
     choice = int(input())
     return choice
 
+
 def main():
     choice = displayScreen()
     while choice != 4:
+
         if choice == 1:
-            bookFlight()
+            first = input("Enter your first name: ")
+            last = input("Enter your last name: ")
+            email = input("Enter your email: ")
+            phone = input("Enter your phone number: ")
+            flight = input("Enter the flight number: ")
+            seat = input("Enter the seat number: ")
+            bookFlight(first, last, email, phone, flight, seat)
+
         elif choice == 2:
-            manageTrip()
+            passenger = input("Enter your passenger id: ")
+            manageTrip(passenger)
+
         elif choice == 3:
-            checkFlightStatus()
+            departure = input("Enter the departure city: ")
+            arrival = input("Enter the arrival city: ")
+            date = input("Enter the departure date: ")
+            checkFlightStatus(departure, arrival, date)
+
         else:
             print("Invalid choice")
+
         choice = displayScreen()
+
+
+if __name__ == "__main__":
+    main()
