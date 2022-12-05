2ea05134f7390e4bc4770e6988c64ff96ee8d585
Miles Pei <mpei36@gmail.com>
2022-12-05 11:14:48 -0600

    insertCustomer

---
+++ procedures/insertCustomer.sql
@@ -0,0 +1,22 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertCustomer`(
+`customerID` INT,
+`fName` VARCHAR(45),
+`lName` VARCHAR(45),
+`dob` DATE,
+`gender` VARCHAR(45),
+`phoneNo` VARCHAR(45),
+`streetAddress` VARCHAR(45),
+`state` VARCHAR(45),
+`country` VARCHAR(45),
+`zipcode` INT,
+`email`  VARCHAR(45),
+`username` VARCHAR(45),
+`password` VARCHAR(45),
+`rewardMiles` INT)
+BEGIN
+
+INSERT INTO customer
+VALUES (customerID, fName, lName, dob, gender, phoneNo, streetAddress, state, country, zipcode, email, username, `password`, rewardMiles);
+END$$
+DELIMITER ;
