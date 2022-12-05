984c3260507f3676a7efa2bf7610be8b6dcff330
Miles Pei <mpei36@gmail.com>
2022-12-05 11:08:36 -0600

    Created "cancelFlight"

---
+++ procedures/cancelFlight.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cancelFlight`(transactionID INT)
+BEGIN
+	# Update occupancies based on transactionID
+    UPDATE seat
+    SET occupancy = 0
+    WHERE seatID IN (
+		SELECT seatID FROM passenger
+		WHERE passengerID in (
+			SELECT passengerID FROM booking
+			WHERE Transaction_transactionID = transactionID
+		)
+	);
+    
+    DELETE FROM booking
+    WHERE Transaction_transactionID = transactionID;
+    
+    DELETE FROM transaction
+    WHERE transactionID = transactionID;
+END$$
+DELIMITER ;
