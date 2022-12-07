a6d759b2e638ca48d4b569bed0f2f4a7f715e87c
Miles Pei <mpei36@gmail.com>
2022-12-06 18:17:37 -0600

    updated CreateFlight

--- procedures/createFlight.sql
+++ procedures/createFlight.sql
@@ -9,9 +9,10 @@ BEGIN
 	INSERT INTO flight
     VALUES (0, 0, departDate, arrivalDate, employeeID, aircraftID);
     
-    CALL createSeats(LAST_INSERT_ID(), 10); 
-    
+    SET @fID = LAST_INSERT_ID();
+    CALL createSeats(@fID, 10);
+
     INSERT INTO airport_has_flight
-    VALUES (departAirport, LAST_INSERT_ID(), arrivalAirport);
+    VALUES (departAirport, @fID, arrivalAirport);
     
 END
